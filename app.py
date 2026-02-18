decb7449a8170b56da730a47a35ef7a7a2c94e57
Sayan Mallick <malsayan6@gmail.com>
2026-02-18 23:22:39 +0530

    first

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+from pypdf import PdfReader
+from dotenv import load_dotenv
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+def get_pdf_text(pdf_docs):
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader=PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+    return text 
+def get_text_chunks(raw_text):
+    text_splitter=RecursiveCharacterTextSplitter(
+         chunk_size=1000,
+         chunk_overlap=200
+    )
+    chunks=text_splitter.split_text(raw_text)
+    return chunks
+  
+def main():
+    load_dotenv()
+
+    st.set_page_config(page_title="Chat with mutiple pdf",  page_icon=":books :")
+
+    st.header("Chat with mutiple pdf :books:")
+    st.text_input("Enter your question here", key="question")
+    
+    with st.sidebar:
+     st.subheader("Your document")
+     pdf_docs=st.file_uploader("Upload your PDF file here", type=["pdf"], key="pdf_file", accept_multiple_files=True)
+     if st.button("Process PDF", key="process_pdf"):
+      with st.spinner("Processing..."): 
+         raw_text=get_pdf_text(pdf_docs)
+        
+         text_chunks=get_text_chunks(raw_text)
+         st.write(text_chunks)
+         
+if __name__ == "__main__":
+  main()
